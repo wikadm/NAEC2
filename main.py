b910d74277bbcdcba2aabe3107da788b0594a40a
Wiktor <wikadm@Wiktors-MacBook-Pro.local>
2025-12-28 15:46:23 +0100

    add main.py to repo

---
+++ main.py
@@ -0,0 +1,11 @@
+####Neural and Evolutionary Computation (NEC) - Assignment 2
+####Wiktor Samulski
+
+import numpy as np
+import random
+import os
+import matplotlib.pyplot as plt
+from typing import List, Tuple, Dict
+
+
+RANDOM_SEED = 42
